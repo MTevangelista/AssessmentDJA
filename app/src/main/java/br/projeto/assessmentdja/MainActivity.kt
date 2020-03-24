78a9e6a3db991cfdc64660f2daf1162261e001fa
Matheus Evangelista <matheusevangelistabr@gmail.com>
2020-03-24 12:52:42 -0300

    Ao clicar no botão de cadastro, o usuário é redirecionado para a tela de cadastro

--- app/src/main/java/br/projeto/assessmentdja/MainActivity.kt
+++ app/src/main/java/br/projeto/assessmentdja/MainActivity.kt
@@ -18,7 +18,8 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         var btnId = view.id
 
         when(btnId){
-           R.id.button_login -> startActivity(Intent(this, CadastroActivity::class.java))
+           R.id.button_login -> startActivity(Intent(this, LoginActivity::class.java))
+            R.id.button_cadastro -> startActivity(Intent(this, button_cadastro::class.java))
         }
     }
 
