ba925ce8e46696ce0c54a0cb8dfc90e64a75cf1d
Matheus Evangelista <matheusevangelistabr@gmail.com>
2020-03-24 12:50:49 -0300

    Ao clicar no botão de login, o usuário é redirecionado para a tela de login

--- app/src/main/java/br/projeto/assessmentdja/MainActivity.kt
+++ app/src/main/java/br/projeto/assessmentdja/MainActivity.kt
@@ -1,12 +1,29 @@
 package br.projeto.assessmentdja
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setListeners()
+    }
+
+    override fun onClick(view: View) {
+        var btnId = view.id
+
+        when(btnId){
+           R.id.button_login -> startActivity(Intent(this, CadastroActivity::class.java))
+        }
+    }
+
+    private fun setListeners(){
+        button_cadastro.setOnClickListener(this)
+        button_login.setOnClickListener(this)
     }
 }
