52f35b1e1034472b80468b754269d03f894c3e57
Matheus Evangelista <matheusevangelistabr@gmail.com>
2020-03-29 12:31:40 -0300

    Cria a classe que representa o RetrofitClient

---
+++ app/src/main/java/br/projeto/assessmentdja/client/RetrofitClient.kt
@@ -0,0 +1,20 @@
+package br.projeto.assessmentdja.client
+
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class RetrofitClient {
+    companion object {
+        private var instance : Retrofit? = null
+        fun getInstance() : Retrofit {
+            if (instance == null){
+                instance = Retrofit.Builder()
+                    .baseUrl("http://infnet.educacao.ws/")
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build()
+            }
+            return instance!!
+        }
+    }
+}
